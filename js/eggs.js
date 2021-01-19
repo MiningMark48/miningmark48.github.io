05fa65f9c754e0cb84d342a7249de5b06c84a8eb
Mark <miningmark48@gmail.com>
2021-01-19 14:08:01 -0500

    More work

---
+++ js/eggs.js
@@ -0,0 +1,11 @@
+function egg_himark(){
+	window.location.replace("https://www.youtube.com/watch?v=aekfPU0SwNw");
+}
+
+function egg_pearcelmod(){
+	alert("Rest in Peace!");
+}
+
+function egg_rr(){
+	window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}

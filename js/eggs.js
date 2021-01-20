1b64ad0499d3be20f68660a0091a344e59e4bdf5
Mark <miningmark48@gmail.com>
2021-01-20 00:35:37 -0500

    Tidal Wave

--- js/eggs.js
+++ js/eggs.js
@@ -1,3 +1,4 @@
+// Main page
 function egg_himark(){
 	window.open("https://www.youtube.com/watch?v=aekfPU0SwNw");
 }
@@ -8,4 +9,13 @@ function egg_pearcelmod(){
 
 function egg_rr(){
 	window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+
+// Tidal Wave
+function egg_crabrave(){
+	window.open("https://www.youtube.com/watch?v=LDU_Txk06tM");
+}
+
+function egg_oceanman(){
+	window.open("https://www.youtube.com/watch?v=6E5m_XtCX3c");
 }

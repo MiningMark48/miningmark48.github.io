a153fdf3bc04d4cc9ad80ac2b3ccb815b80b979d
Mark <miningmark48@gmail.com>
2021-01-22 23:32:06 -0500

    Kaosjr name update

--- js/eggs.js
+++ js/eggs.js
@@ -16,6 +16,10 @@ function egg_crabrave(){
 	window.open("https://www.youtube.com/watch?v=LDU_Txk06tM");
 }
 
+function egg_jb(){
+	window.open("https://www.jbweld.com/");
+}
+
 function egg_oceanman(){
 	window.open("https://www.youtube.com/watch?v=6E5m_XtCX3c");
 }
